d4e6e420caaa4f19b0e84860dded61d490fded1e
Ariq Ikbar Darwansyah <92252295+ariqikbard@users.noreply.github.com>
2022-05-26 19:13:07 +0700

    Update sidebar

--- beasiswa-beasa/resources/views/layouts/partials/sidebar.blade.php
+++ beasiswa-beasa/resources/views/layouts/partials/sidebar.blade.php
@@ -2,9 +2,13 @@
 
     <!-- Add Sidebar Menu Items Here -->
     <x-maz-sidebar-item name="Home" :link="route('home')" icon="bi bi-grid-fill"></x-maz-sidebar-item>
-    @auth
-        @if (Auth::user()->role == 'admin')
+   @auth
+        @if (Auth::user()->role == 'admin' || Auth::user()->role == 'campuss')
             <x-maz-sidebar-item name="Scholarship" :link="route('scholarship')" icon="bi bi-archive-fill"></x-maz-sidebar-item>
+        @endif
+        @if (Auth::user()->role == 'user' )
+            <x-maz-sidebar-item name="File" :link="route('file')" icon="bi bi-list-task"></x-maz-sidebar-item>    
+            <x-maz-sidebar-item name="My Scholarship" :link="route('my')" icon="bi bi-briefcase-fill"></x-maz-sid
         @endif
     @endauth
 
