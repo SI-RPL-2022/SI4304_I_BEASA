8390e5d5e65a2c6f8b07a95d7424a712c7584b94
nurliyanfaris <92175201+nurliyanfaris@users.noreply.github.com>
2022-06-19 20:13:45 +0700

    backend feedback

--- beasiswa-beasa/app/Http/Controllers/FeedbackController.php
+++ beasiswa-beasa/app/Http/Controllers/FeedbackController.php
@@ -3,11 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Feedback;
+use Illuminate\Support\Facades\Auth;
 
 class FeedbackController extends Controller
 {
     public function index()
     {
         return view('feedback.view');
+        $feedbacks = Feedback::all();
+        return view('feedback.view', compact('feedbacks'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'experience' => 'required',
+            'comments' => 'required',
+        ]);
+
+        $feedback = new Feedback();
+        if (Auth::check()) {
+            $feedback->id_user = Auth::user()->id;
+        }
+        if ($request->has('name')) {
+            $feedback->name = $request->name;
+        }
+        if ($request->has('email')) {
+            $feedback->email = $request->email;
+        }
+        $feedback->rate = $request->experience;
+        $feedback->feedback = $request->comments;
+        $feedback->save();
+        // return $feedback;
+        return redirect()->route('feedback')->with('success', 'Feedback added successfully');
     }
 }
